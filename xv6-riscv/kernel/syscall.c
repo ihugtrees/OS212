08cd9799b36bae9a309a47c1a33286d22078a893
nivdavidian <nivdavidian@gmail.com>
2021-03-29 21:27:18 +0300

    syscall.c changes

--- xv6-riscv/kernel/syscall.c
+++ xv6-riscv/kernel/syscall.c
@@ -131,15 +131,58 @@ static uint64 (*syscalls[])(void) = {
 [SYS_trace]   sys_trace,
 };
 
+static char* sysNames[64] = {
+[SYS_fork]    "fork",
+[SYS_exit]    "exit",
+[SYS_wait]    "wait",
+[SYS_pipe]    "pipe",
+[SYS_read]    "read",
+[SYS_kill]    "kill",
+[SYS_exec]    "exec",
+[SYS_fstat]   "fstat",
+[SYS_chdir]   "chdir",
+[SYS_dup]     "dup",
+[SYS_getpid]  "getpid",
+[SYS_sbrk]    "sbrk",
+[SYS_sleep]   "sleep",
+[SYS_uptime]  "uptime",
+[SYS_open]    "open",
+[SYS_write]   "write",
+[SYS_mknod]   "mknod",
+[SYS_unlink]  "unlink",
+[SYS_link]    "link",
+[SYS_mkdir]   "mkdir",
+[SYS_close]   "close",
+[SYS_trace]   "trace",
+};
+
 void
 syscall(void)
 {
   int num;
   struct proc *p = myproc();
+  p->mask = (1<<SYS_kill | 1<<SYS_sbrk);
 
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
+    int arg0 = p->trapframe->a0; // SAVE THE FIRST ARGUMENT
     p->trapframe->a0 = syscalls[num]();
+
+    if(1<<num & p->mask){
+      if(num == SYS_fork){
+        printf("\n%d: syscall %s NULL->%d\n", p->pid, sysNames[num], p->trapframe->a0);
+      }
+
+      else if(num == SYS_kill){
+        printf("\n%d: syscall %s %d->%d\n", p->pid, sysNames[num],arg0, p->trapframe->a0);
+      }
+      else if(num == SYS_sbrk){
+        printf("\n%d: syscall %s %d->%d\n", p->pid, sysNames[num],arg0, p->trapframe->a0);
+      }
+      else {
+        printf("\n%d: syscall %s ->%d\n", p->pid, sysNames[num], p->trapframe->a0);
+      }
+    }
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
