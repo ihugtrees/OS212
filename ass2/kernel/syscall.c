02ea40c77c533e7de69959a1a25c3da3455c31da
ihugtrees <igoralikin@gmail.com>
2021-04-19 19:37:08 +0300

    2.1.3 partial

--- ass2/kernel/syscall.c
+++ ass2/kernel/syscall.c
@@ -104,6 +104,8 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_wait(void);
 extern uint64 sys_write(void);
 extern uint64 sys_uptime(void);
+extern uint64 sys_sigprocmask(void);
+extern uint64 sys_sigaction(void);
 
 static uint64 (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -127,6 +129,8 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_sigprocmask]   sys_sigprocmask,
+[SYS_sigaction]   sys_sigaction,
 };
 
 void
--- ass2/user/user.h
+++ ass2/user/user.h
@@ -23,6 +23,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+uint sigprocmask (uint sigmask);
+// int sigaction (int signum, const struct sigaction *act, struct sigaction *oldact);
 
 // ulib.c
 int stat(const char*, struct stat*);
